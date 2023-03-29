d318acd121ecb8d9e1f909bdc0332f981926090c
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-03-29 06:41:24 +0400

    c# introduction

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+
+namespace lesson2
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //int number = 8;
+            //Console.WriteLine(number);
+
+
+
+            //double b = 565464.999796;
+            //Console.WriteLine(b);
+
+            //bool b = false;
+            //Console.WriteLine(b);
+
+            //char b= 'A';
+            //Console.WriteLine(c);
+
+            //string b = "game developer";
+            //Console.WriteLine(b);
+
+            //Task2
+            //float gamer = 23f;
+            //int mobile;
+            //mobile=(int)gamer;
+            //Console.WriteLine(mobile);
+
+
+            //double num = 43596.527826;
+            //int developer;
+            //developer= (int)num;
+            //Console.WriteLine(developer);
+
+
+            //Task3
+            //Console.WriteLine("enter your number");
+            //int number = 0;
+            //number =Convert.ToInt32(Console.ReadLine());
+            //if (number >= 0)
+            //{
+            //    Console.WriteLine("this number pozitive");
+            //}
+            //else if(number <= 0)
+            //{
+            //    Console.WriteLine("this number negative");
+            //}
+
+
+
+            //Task4
+            // Console.WriteLine("Enter your age:");
+            // int age = 0;
+            // age=Convert.ToInt32(Console.ReadLine());
+            //if (age >18)
+            // {
+            //    Console.WriteLine("your age enough to vote");
+            // }
+            //else if(age < 18)
+            // {
+            //     Console.WriteLine("your age not enough to vote");
+            // }
+
+
+            //Task5
+            //int number = 1;
+            //while (number <= 100)
+            //{
+            //   if(number%2==0)
+            //        Console.WriteLine(number);
+            //   number++;
+            //}
+
+            //for(int number = 1; number <= 100; number++)
+            //{ 
+            //  if(number%2==0)
+            //        Console.WriteLine(number);
+            //}
+
+
+            //Task6 
+
+            //Console.WriteLine("Enter your number:");
+            //for(int number=1;number<=10;number++) 
+            //{ 
+            //   for(int num2=1; num2 <= 10; num2++)
+            //    {
+            //        Console.WriteLine("{0}*{1}={2}", number, num2, (number * num2));
+            //    }
+            //}
+
+
+
+            //Task7
+
+            
+            //int password = 1808;
+            //while (true) 
+            //{
+            //    Console.WriteLine("enter your password");
+            //    password=Convert.ToInt32(Console.ReadLine());
+            //    if(password == 0)
+            //    {
+            //        break;
+            //    }
+            //}
+
+
+
+
+        }
+    }
+}
